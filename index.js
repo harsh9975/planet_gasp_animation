ac3d5a3dcf4e87d289018b7f1a2f7f5580da8f5e
harsh9975 <gautam33776@gmail.com>
2020-11-28 00:18:52 +0530

    Done

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
 //Parallax
-
 let scene = document.getElementById("scene");
 let parallaxInstance = new Parallax(scene);
+
+//SWIPER
+let keys = [
+    "Mercury",
+    "Venus",
+    "Earth",
+    "Mars",
+    "Jupiter",
+    "Saturn",
+    "Uranus",
+    "Neputne"
+];
+
+let slider = new Swiper(".swiper-container",{
+    slidesPerView: "auto",
+    spaceBetween: 150,
+    centeredSlides: true,
+    mousewheel: true,
+    pagination:{
+        el: ".planet-links",
+        clickable: true,
+        renderBullet: function(index, className){
+            return '<div class="'+ className + '">' + keys[index] + "</div>"
+        }
+    }
+});
